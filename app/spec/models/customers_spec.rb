b53aa521c139bcca53ba4ba7242c60653f8380f9
Renington Neri <renington.neri@gmail.com>
2018-06-02 19:01:24 -0300

    Add attributes_for

--- app/spec/models/customers_spec.rb
+++ app/spec/models/customers_spec.rb
@@ -29,5 +29,11 @@ describe Customer, type: :model do
         expect(customer.vip).to eq(true)
     end
 
+    it 'attributes_for' do
+        attrs = attributes_for(:customer)
+        customer = Customer.create(attrs)
+        expect(customer.full_name).to start_with('Sr. ')
+    end
+
     it { expect{ create(:customer) }.to change{Customer.all.size}.by(1) }
 end
