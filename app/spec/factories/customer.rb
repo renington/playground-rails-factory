1460bae768d2f868ff0d7c53340bf789d17980de
Renington Neri <renington.neri@gmail.com>
2018-06-02 19:18:54 -0300

    Add attribute transient

--- app/spec/factories/customer.rb
+++ app/spec/factories/customer.rb
@@ -1,5 +1,10 @@
 FactoryBot.define do
     factory :customer do
+
+        transient do
+            upcased false
+        end
+
         name Faker::Name.name
         email Faker::Internet.email
         vip false
@@ -8,6 +13,10 @@ FactoryBot.define do
         factory :customer_vip do
             vip true
             days_to_pay 30
+        end
+
+        after(:create) do |customer, evaluator|
+            customer.name.upcase! if evaluator.upcased
         end
 
     end
