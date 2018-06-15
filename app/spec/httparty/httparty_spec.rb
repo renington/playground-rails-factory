1da4f43120b74a8e82e0065d5f28e8c81fa1bdf6
Renington Neri <renington.neri@gmail.com>
2018-06-14 22:06:30 -0300

    Add a test

--- app/spec/httparty/httparty_spec.rb
+++ app/spec/httparty/httparty_spec.rb
@@ -1,11 +1,14 @@
 require 'httparty'
+require 'VCR'
 
 describe 'HTTParty' do
     it 'content-type' do
       stub_request(:get, "https://jsonplaceholder.typicode.com/posts/3").to_return(status: 200, body: "", headers: { 'content-type' : 'application/json'})
 
-      response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/3")
-      content_type = response.headers['content-type']
-      expect(content_type).to match(/application\/json/)
+      VCR.use_cassette('jsonplaceholder/posts') do
+        response = HTTParty.get("https://jsonplaceholder.typicode.com/posts/3")
+        content_type = response.headers['content-type']
+        expect(content_type).to match(/application\/json/)
+       end
     end
 end
