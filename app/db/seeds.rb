ed0b0cfed3ba08c25805713970ac9dba14e38eca
Renington Neri <renington.neri@gmail.com>
2018-06-15 18:28:01 -0300

    Add seed example

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# 10.times do |i|
+#     Contact.create! (
+#         name: Faker::Name.name,
+#         email: Faker::Internet.email,
+#         kind: Kind.all.sample,
+#         rmk: Faker::Lorem.paragraph(2)
+#     )
+# end
+
+# PUTS "Generating Users. .."
